134882bb7abca32b409df911ff5fd1b7e4e66c98
Florian Suen <dipars@gmail.com>
2023-01-23 22:09:24 +0800

    I changed from only using the qrcode base64 ref with filesystem to taking a screenshot of the view using view shot. I wanted to also take an image of the user details, so when the user desides to share, it will be apparent who they're adding.

--- src/screens/QRCodeScreen.tsx
+++ src/screens/QRCodeScreen.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { PropsWithChildren, useContext, useRef, useState } from "react";
 import {
   StyleSheet,
   View,
@@ -8,7 +8,7 @@ import {
   Button,
 } from "react-native";
 import * as FileSystem from "expo-file-system";
-
+import ViewShot, { captureScreen } from "react-native-view-shot";
 import QRCode from "react-native-qrcode-svg";
 import { userContext } from "../../utility/userAuth";
 import { useAppSelector } from "../../utility/useReduxHooks";
@@ -25,47 +25,95 @@ interface QREncode {
 }
 
 export const QRCodeScreen = () => {
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-
-  let qrCode: QREncode | null = null;
+  const ref: any = useRef();
   const userAuth = useContext(userContext);
   const payload: QRCodeProps = { inviteId: "Something", name: "New" };
   const currentUser = useAppSelector((state) =>
     state.contacts.contacts.find((item) => item.id === userAuth?.attributes.sub)
   )!;
 
+  const saveImageHandler = async () => {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    ref.current.capture().then((uri: any) => {
+      status === "granted" &&
+        MediaLibrary.saveToLibraryAsync(uri).then(() => {
+          ToastAndroid.show("Saved to Gallery", ToastAndroid.SHORT);
+        });
+    });
+  };
+
+  const QRMainComponent = ({ screenshot }: { screenshot: boolean }) => {
+    return (
+      <View>
+        {screenshot ? (
+          <LinearGradient
+            colors={["purple", "orange"]}
+            start={{ x: 0.8, y: 0.35 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <View style={styles.qrCode}>
+              {
+                <QRCode
+                  size={240}
+                  color="white"
+                  logo={{ uri: currentUser.image }}
+                  backgroundColor="transparent"
+                  value={JSON.stringify(payload)}
+                />
+              }
+            </View>
+            <View style={styles.textInfo}>
+              <Text>Scan QR code to form a contract</Text>
+              <Image source={{ uri: currentUser.image }} />
+              <Text>{currentUser.username}</Text>
+            </View>
+          </LinearGradient>
+        ) : (
+          <>
+            <View style={styles.qrCode}>
+              {
+                <QRCode
+                  size={240}
+                  color="white"
+                  logo={{ uri: currentUser.image }}
+                  backgroundColor="transparent"
+                  value={JSON.stringify(payload)}
+                />
+              }
+            </View>
+            <View style={styles.textInfo}>
+              <Text>Scan QR code to form a contract</Text>
+              <Image source={{ uri: currentUser.image }} />
+              <Text>{currentUser.username}</Text>
+            </View>
+            <Button
+              title="Save QR Code"
+              onPress={() => {
+                saveImageHandler();
+              }}
+            />
+          </>
+        )}
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <LinearGradient
         colors={["purple", "orange"]}
         style={styles.linearGradient}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 2, y: 1 }}
       >
-        <View style={styles.qrCode}>
-          <QRCode
-            size={250}
-            color="white"
-            logo={{ uri: currentUser.image }}
-            backgroundColor="transparent"
-            value={JSON.stringify(payload)}
-            getRef={(qrCode64) => {
-              qrCode = qrCode64;
-            }}
-          />
-        </View>
-        <View style={styles.info}>
-          <Text>Scan QR code to form a contract</Text>
-          <Image source={{ uri: currentUser.image }} />
-          <Text>{currentUser.username}</Text>
-        </View>
-        <Button
-          title="Save QR Code"
-          onPress={async () => {
-            const { status } = await MediaLibrary.requestPermissionsAsync();
-            status === "granted" && qrCode ? saveQrToDisk(qrCode) : null;
-          }}
-        />
+        <ViewShot
+          style={{ position: "absolute", top: -500 }}
+          ref={ref}
+          options={{ fileName: "Your-File-Name", format: "jpg", quality: 0.9 }}
+        >
+          <QRMainComponent screenshot={true} />
+        </ViewShot>
+        <QRMainComponent screenshot={false} />
       </LinearGradient>
     </View>
   );
@@ -75,22 +123,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  qrCode: { paddingVertical: 30, paddingHorizontal: 50 },
   linearGradient: { flex: 1, alignItems: "center", justifyContent: "center" },
-  qrCode: { position: "absolute", top: 170 },
-  info: { marginTop: 50, alignItems: "center" },
+  textInfo: { alignItems: "center" },
 });
-
-const saveQrToDisk = (qrCode: QREncode) => {
-  qrCode.toDataURL((data: string) => {
-    const filename = FileSystem.documentDirectory + "my-qr.png";
-    FileSystem.writeAsStringAsync(filename, data, {
-      encoding: FileSystem.EncodingType.Base64,
-    })
-      .then((success) => {
-        return MediaLibrary.saveToLibraryAsync(filename);
-      })
-      .then(() => {
-        ToastAndroid.show("Saved to Gallery", ToastAndroid.SHORT);
-      });
-  });
-};
