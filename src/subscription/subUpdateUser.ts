28c61d6556e27210472e0e079ff3d7dafb7f60ae
Florian Suen <dipars@gmail.com>
2023-03-24 22:03:03 +0800

    fixed subscription error

--- src/subscription/subUpdateUser.ts
+++ src/subscription/subUpdateUser.ts
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { graphqlOperation, API } from "aws-amplify";
 import { onUpdateUser } from "../graphql/subscriptions";
 import { updateContactStatus } from "../redux/contactList/contactListSlice";
@@ -8,29 +10,30 @@ export const subOnupdateUser = (
   currentUserId: string,
   dispatch: AppDispatch
 ) => {
-  const onUpdateCurrentUser = API.graphql(
-    graphqlOperation(onUpdateUser, {
-      filter: { not: { id: { eq: currentUserId } } },
-    })
-  );
-  const onUpdateCurrentUserSubscription =
-    "subscribe" in onUpdateCurrentUser &&
-    onUpdateCurrentUser.subscribe({
-      next: ({ value }: any) => {
-        dispatch(
-          updateContactStatus({
-            _version: value.data.onUpdateUser._version,
-            status: value.data.onUpdateUser.status,
-            id: value.data.onUpdateUser.id,
-          })
-        );
-      },
-      error: (err) => console.log(`onUpdateCurrentUserStatus Error ${err}`),
-    });
+  useEffect(() => {
+    const onUpdateCurrentUser = API.graphql(graphqlOperation(onUpdateUser));
+    const onUpdateCurrentUserSubscription =
+      "subscribe" in onUpdateCurrentUser &&
+      onUpdateCurrentUser.subscribe({
+        next: ({ value }: any) => {
+          value.data.onUpdateUser.id !== currentUserId &&
+            dispatch(
+              updateContactStatus({
+                _version: value.data.onUpdateUser._version,
+                status: value.data.onUpdateUser.status,
+                id: value.data.onUpdateUser.id,
+              })
+            );
+        },
+        error: (err) => {
+          console.log(`onUpdateCurrentUserStatus Error ${err}`);
+        },
+      });
 
-  return () => {
-    console.log("Unsubscribing onUpdateCurrentUserStatus");
-    onUpdateCurrentUserSubscription &&
-      onUpdateCurrentUserSubscription.unsubscribe();
-  };
+    return () => {
+      console.log("Unsubscribing UpdateCurrentUserStatus");
+      onUpdateCurrentUserSubscription &&
+        onUpdateCurrentUserSubscription.unsubscribe();
+    };
+  }, []);
 };
